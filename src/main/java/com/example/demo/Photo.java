91c3077af608253b13dd282d400107607168332c
idrissmiraoui <miraoui.idriss@gmail.com>
2021-04-15 14:25:52 +0200

    a

---
+++ src/main/java/com/example/demo/Photo.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "PHOTO")
+public class Photo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int voteNumber;
+
+    private int position;
+
+    private String photoName;
+
+    private String photoExtension;
+
+}
